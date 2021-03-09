f8819f6adf1b184e702f0cbfdfe454f7bf4f0757
Tim Anderson <tdanderson805@gmail.com>
2021-03-08 22:05:48 -0800

    added tests for db filling script

--- db/dbFiller.js
+++ db/dbFiller.js
@@ -1,47 +1,12 @@
 var Promise = require('bluebird');
-const LoremIpsum = Promise.promisifyAll(require("lorem-ipsum").LoremIpsum);
 var UsernameGenerator = Promise.promisifyAll(require('username-generator'));
 var db = Promise.promisifyAll(require('../db'));
+var lorem = require('./randomWordGeneration');
+var random = require('./randomMathGeneration')
 
 var fs = require('fs');
 var linkArray = fs.readFileSync(`${__dirname}/../bucketLinks/list.txt`).toString().split('\n');
 
-//random text configuration
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
-
-//random generation
-var generateInbetweenSync = (max) => {
-  return Math.ceil(Math.random() * max)
-};
-
-var threeValProb = (num1, num2) => {
-  var random = Math.random()
-    if (random <= num1) {
-      return 0
-    } else if (random <= num2) {
-      return 1
-    } else {
-      return 2
-    }
-}
-
-var twoValProb = (num1) => {
-  var random = Math.random()
-    if (random <= num1) {
-      return 0
-    }  else {
-      return 1
-    }
-}
 
 //limit the number of querys persecond to db
 var makeEverythingWait = () => {
@@ -57,12 +22,12 @@ var makeEverythingWait = () => {
   for (var i = 0; i < 250; i++) {
     var username = await UsernameGenerator.generateUsername()
     var profileLink = linkArray[i];
-    var userTheme = generateInbetweenSync(10)
-    var steamLevel = generateInbetweenSync(100)
-    var reviewsGiven = generateInbetweenSync(20)
-    var gamesOwned = generateInbetweenSync(25)
-    var playTime = generateInbetweenSync(80)
-    var activation = generateInbetweenSync(2)
+    var userTheme = random.generateInbetweenSync(10)
+    var steamLevel = random.generateInbetweenSync(100)
+    var reviewsGiven = random.generateInbetweenSync(20)
+    var gamesOwned = random.generateInbetweenSync(25)
+    var playTime = random.generateInbetweenSync(80)
+    var activation = random.generateInbetweenSync(2)
 
     await db.query(`insert into users (
       userName,
@@ -83,20 +48,21 @@ var makeEverythingWait = () => {
         )`)
   }
 
+  console.log(lorem)
   //reviews
   for (var i = 0; i < 2500; i++) {
-    var randomUser = generateInbetweenSync(250)
-    var reviewTextSize = generateInbetweenSync(10)
-    var reviewText = await lorem.generateParagraphs(reviewTextSize)
-    var creationDate = generateInbetweenSync(31536000)
-    var positiveReview = twoValProb(.1)
-    var helpfulCount = generateInbetweenSync(50)
-    var notHelpfulCount = threeValProb(.8, .95)
-    var funnyCount = threeValProb(.7, .8)
-    var comments = threeValProb(.7, .9)
-    var earlyAccess = twoValProb(.9)
-    var awards = generateInbetweenSync(20)
-    var game = generateInbetweenSync(100)
+    var randomUser = random.generateInbetweenSync(250)
+    var reviewTextSize = random.generateInbetweenSync(10)
+    var reviewText = await lorem.lorem.generateParagraphs(reviewTextSize)
+    var creationDate = (1615265125152 - random.generateInbetweenSync(31536000))
+    var positiveReview = random.twoValProb(.1)
+    var helpfulCount = random.generateInbetweenSync(50)
+    var notHelpfulCount = random.threeValProb(.8, .95)
+    var funnyCount = random.threeValProb(.7, .8)
+    var comments = random.threeValProb(.7, .9)
+    var earlyAccess = random.twoValProb(.9)
+    var awards = random.generateInbetweenSync(20)
+    var game = random.generateInbetweenSync(100)
 
     await db.query(`insert into reviews (
       userID,
