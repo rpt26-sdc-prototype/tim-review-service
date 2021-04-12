b62268f3a88f30bb06e13233ce6626ed3fc0dd31
Tim Anderson <tdanderson805@gmail.com>
2021-04-11 21:42:42 -0700

    docker and enzyme refactor

--- db/index.js
+++ db/index.js
@@ -2,7 +2,8 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   host: process.env.MYSQL_URL || 'localhost',
-  user: 'root',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'service1'
 });
 
