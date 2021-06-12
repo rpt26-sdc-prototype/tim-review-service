3c100cc96fcad324374a9f31415ddfd38a4a98cf
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-12 15:18:20 -0600

    pm2 process for review-server

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  apps: [
-    {
-      name: "Main Server",
-      script: "index.js",
-      instances: "1",
-      exec_mode: "cluster",
-      env: {
-        NODE_ENV: "development",
-        PORT: 4000
-      }
-    },
-  ]
+  apps: [{
+    name: 'review-server',
+    script: './server/index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-116-228-112.us-east-2.compute.amazonaws.com',
+      key: './Login_Scripts/reviews-server.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:rpt26-sdc-prototype/tim-review-service.git',
+      path: '/home/ubuntu/review-server',
+      'post-deploy': 'npm install && pm2 startOrRestart ./server/ecosystem.config.js'
+    }
+  }
 }
