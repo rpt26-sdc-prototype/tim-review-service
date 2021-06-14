9fa5c84e0dcad17ce71b7fd2b6b6b099bb323651
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-14 14:25:07 -0600

    trying out two review-servers

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -6,7 +6,10 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-116-228-112.us-east-2.compute.amazonaws.com',
+      host: [
+        'ec2-18-116-228-112.us-east-2.compute.amazonaws.com',
+        'ec2-3-20-97-156.us-east-2.compute.amazonaws.com',
+      ],
       key: './Login_Scripts/reviews-server.pem',
       ref: 'origin/master',
       repo: 'git@github.com:rpt26-sdc-prototype/tim-review-service.git',
